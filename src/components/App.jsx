6846b13b319fabac931fefeba18130743937a889
xtrphy <perepeckinvlad@gmail.com>
2025-03-22 20:29:09 +0500

    Add general cv section

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import General from './General'
+import '../styles/App.css'
+import '../styles/Main.css'
+
+export default function App() {
+
+  return (
+    <>
+      <h1 className='your-cv'>Resume</h1>
+      <General />
+    </>
+  );
+}
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from '../components/App.jsx'
+import '../styles/Main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
